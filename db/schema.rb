97f4f6029225792aae0c9aa519c31bd638d94e5b
Ismael-Soares bellsoares3@gmail.com <bellsoares3@gmail.com>
2023-11-28 18:30:01 -0300

    merge

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_28_153243) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_28_185921) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,14 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_28_153243) do
     t.datetime "updated_at", null: false
     t.index ["experience_id"], name: "index_appointments_on_experience_id"
     t.index ["user_id"], name: "index_appointments_on_user_id"
+  end
+
+  create_table "carousel_items", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "experiences", force: :cascade do |t|
