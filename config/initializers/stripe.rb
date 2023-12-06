58c38c4f1ff949b030395b42cdf054b11209f2ea
Benjamin Tayar <tayarbenjamin@gmail.com>
2023-12-06 17:12:51 -0300

    solved

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,12 +1,12 @@
-Rails.configuration.stripe = {
-  publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
-  secret_key:      ENV['STRIPE_SECRET_KEY'],
-  signing_secret:  ENV['STRIPE_WEBHOOK_SECRET_KEY']
-}
+# Rails.configuration.stripe = {
+#   publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
+#   secret_key:      ENV['STRIPE_SECRET_KEY'],
+#   signing_secret:  ENV['STRIPE_WEBHOOK_SECRET_KEY']
+# }
 
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
-StripeEvent.signing_secret = Rails.configuration.stripe[:signing_secret]
+# Stripe.api_key = Rails.configuration.stripe[:secret_key]
+# StripeEvent.signing_secret = Rails.configuration.stripe[:signing_secret]
 
-StripeEvent.configure do |appointments|
-  events.subscribe 'checkout.session.completed', StripeCheckoutSessionService.new
-end
+# StripeEvent.configure do |appointments|
+#   appointments.subscribe 'checkout.session.completed', StripeCheckoutSessionService.new
+# end
