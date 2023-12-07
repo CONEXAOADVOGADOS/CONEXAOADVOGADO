7f34d2898b93c297747771434c6feeb2c7873e32
Fred Franca <fredfranssa@gmail.com>
2023-12-07 15:24:11 -0300

    heroku

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -15,31 +15,11 @@ class AppointmentsController < ApplicationController
   end
 
   def create
-    experience = Experience.find(params[:experience_id])
-    @appointment = Appointment.create!(experience: experience, amount: experience.price * 100, state: "pending", user: current_user)
+    @experience = Experience.find(params[:experience_id])
+    @appointment = Appointment.create(experience: @experience, amount: @experience.price * 100, state: "pending", user: current_user)
     # redirect_to appointments_path, notice: 'Appointment created successfully.'
     authorize @appointment
-
-    session = Stripe::Checkout::Session.create(
-      payment_method_types: ['card'],
-      line_items: [{
-        price_data: {
-          currency: 'usd',
-          unit_amount: @appointment.amount,
-          product_data: {
-            name: @experience.category,
-            description: @experience.description,
-            images: [@experience.photo&.url],
-          },
-        },
-        quantity: 1,
-      }],
-      mode: 'payment',
-      success_url: appointment_url(@appointment),
-      cancel_url: appointment_url(@appointment)
-    )
-
-    @appointment.update(checkout_session_id: session.id)
+    create_session
     redirect_to new_appointment_payment_path(@appointment)
   end
 
@@ -68,6 +48,27 @@ class AppointmentsController < ApplicationController
   end
 
   private
+
+  def create_session
+    session = Stripe::Checkout::Session.create(payment_method_types: ['card'],
+      line_items: [{
+        price_data: {
+          currency: 'usd',
+          unit_amount: @appointment.amount,
+          product_data: {
+            name: @experience.category,
+            description: @experience.description,
+            images: [@experience.photo&.url],
+          },
+        },
+        quantity: 1,
+      }],
+      mode: 'payment',
+      success_url: appointment_url(@appointment),
+      cancel_url: experience_url(@experience))
+    @appointment.checkout_session_id = session.id
+    @appointment.save!
+  end
 
   def set_event
     @experience = Experience.find(params[:experience_id])
--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,5 +1,4 @@
 class PaymentsController < ApplicationController
-
   def new
     @appointment = current_user.appointments.where(state: 'pending').find(params[:appointment_id])
     authorize @appointment
