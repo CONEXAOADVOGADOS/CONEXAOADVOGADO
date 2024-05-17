0b88f6c9499a273e0c8bf91bbf6afe52108196e0
Fred Franca <fredfranssa@gmail.com>
2024-05-17 17:42:04 -0300

    rackup

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Mywelcome
+module CONEXAOADVOGADOS
   class Application < Rails::Application
     config.generators do |generate|
       generate.assets false
