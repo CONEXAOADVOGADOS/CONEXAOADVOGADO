a6e38e55b7a09ecdc5cedab30e4e997efe41d1e4
Felipe Abdo <felipe.abdo.ti@gmail.com>
2024-05-21 14:21:06 -0300

    feat: registro da navbar no stimulus

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,6 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+import NavbarController from './navbar_controller'
+Stimulus.register('navbar', NavbarController)
